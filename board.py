af2466dd543d85daa7898dcb7b21e87332feb480
isabellabonilla <isabellabonilla@ufl.edu>
2023-12-05 11:15:40 -0500

    added board class template

---
+++ board.py
@@ -0,0 +1,64 @@
+import sudoku_generator
+import cell
+
+class Board(Cell):
+
+    def __init__(self, width, height, screen, difficulty):
+        # screen is a window from PyGame.
+        # difficulty is a variable to indicate if the user chose easy, medium, or hard
+        self.difficulty = difficulty
+
+    def draw(self):
+        # draws an outline of the Sudoku grid, with bold lines to delineate the 3x3 boxes.
+        # draws every cell on this board
+        pass
+
+    def select(self, row, col):
+        # marks the cell at (row, col) in the board as the current selected cell.
+        # Once a cell has been selected, the user can edit its value or sketched value.
+        pass
+
+    def click(self, x, y):
+        # if a tuple of (x, y) coordinates is within the displayed board, this function returns a tuple of the (row, col)
+        # of the cell which was clicked. Otherwise, this function returns None.
+        pass
+
+    def clear(self):
+        # Clears the value cell. Note that the user can only remove the cell values and sketched value that are
+        # filled by themselves
+        pass
+
+    def sketch(selfself, value):
+        # Sets the sketched value of the current selected cell equal to user entered value.
+        # It will be displayed at the top left corner of the cell using the draw() function.
+        pass
+
+    def place_number(self, value):
+        # Sets the value of the current selected cell equal to user entered value.
+        # Called when the user presses the Enter key
+        pass
+
+    def reset_to_original(self):
+        # Reset all cells in the board to their original values (0 if cleared, otherwise the corresponding digit)
+        pass
+
+    def is_full(self):
+        # Returns a Boolean value indicating whether the board is full or not.
+        pass
+
+    def update_board(self):
+        # Updates the underlying 2D board with the values in all cells.
+        pass
+
+    def find_empty(self):
+        # Finds an empty cell and returns its row and col as a tuple (x, y)
+        pass
+
+    def check_board(self):
+        # Check whether the Sudoku board is solved correctly.
+        pass
+
+
+
+
+
