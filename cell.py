b80c317b0819729bf3fd5d04426ad29f3e8b7b02
isabellabonilla <isabellabonilla@ufl.edu>
2023-12-05 11:26:51 -0500

    added cell class and complete all methods except draw

---
+++ cell.py
@@ -0,0 +1,27 @@
+import sudoku_generator
+
+class Cell:
+
+    def __init__(self, value, row, col, screen):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+        self.sketched_value = 0
+
+    def set_cell_value(self, value):
+        # setter for this cell’s value
+
+        self.value = value
+
+    def set_sketched_value(self, value):
+        # Setter for this cell’s sketched value
+
+        self.sketched_value = value
+
+    def draw(self):
+        # draws this cell, along with the value inside it.
+        # if this cell has a nonzero value, that value is displayed.
+        # otherwise, no value is displayed in the cell.
+        # the cell is outlined red if it is currently selected.
+
