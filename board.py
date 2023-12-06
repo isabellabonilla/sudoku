835104620619bdd3e9c1f711b35524bedc05d511
gjbernstein <github@ginabernstein.com>
2023-12-05 21:19:32 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	board.py

--- board.py
+++ board.py
@@ -1,27 +1,16 @@
-from sudoku_generator import generate_sudoku
+import sudoku_generator
 from cell import Cell
 import pygame, sys
 
-class Board:
-    def __init__(self, width, height, screen, num_removed):
+class Board(Cell):
+
+    def __init__(self, width, height, screen, difficulty):
         # screen is a window from PyGame.
         # difficulty is a variable to indicate if the user chose easy, medium, or hard
+        self.difficulty = difficulty
         self.width = width
         self.height = height
         self.screen = screen
-        sudoku_array = generate_sudoku(9,num_removed)
-        self.cells = []
-        self.editable_cells = []
-        for i in range(height): #each row
-            cell_row = []
-            for j in range(width): #each column
-                # append a cell with the same stuff as the sudoku array
-                cell_row.append(Cell(sudoku_array[i][j], i, j, self.screen))
-                if sudoku_array[i][j] == 0:
-                    self.editable_cells.append((i, j)) #append the coordinates of the editable cells
-            self.cells.append(cell_row)
-
-
 
     def draw(self):
         # draws an outline of the Sudoku grid, with bold lines to delineate the 3x3 boxes.
@@ -43,6 +32,36 @@ class Board:
             pygame.draw.line(self.screen, (0, 0, 0), (0, i * (self.height // 9)),
                              (self.width, i * (self.height // 9)), thickness)
 
+        # initialize button font
+        button_font = pygame.font.Font(None, 20)
+
+        # initialize buttons
+        # initialize text first
+        reset_text = button_font.render("Reset", 0, (255, 255, 255))
+        restart_text = button_font.render("Restart", 0, (255, 255, 255))
+        quit_text = button_font.render("Quit", 0, (255, 255, 255))
+
+        # initialize button background color and text
+        reset_surface = pygame.Surface((reset_text.get_size()[0] + 20, reset_text.get_size()[1] + 20))
+        reset_surface.fill((0, 176, 188))
+        reset_surface.blit(reset_text, (10, 10))
+
+        restart_surface = pygame.Surface((restart_text.get_size()[0] + 20, restart_text.get_size()[1] + 20))
+        restart_surface.fill((0, 176, 188))
+        restart_surface.blit(restart_text, (10, 10))
+
+        quit_surface = pygame.Surface((quit_text.get_size()[0] + 20, quit_text.get_size()[1] + 20))
+        quit_surface.fill((0, 176, 188))
+        quit_surface.blit(quit_text, (10, 10))
+
+        # initialize button rectangle
+        reset_rectangle = reset_surface.get_rect(center=(self.width // 4, 9 * self.height // 10))
+        restart_rectangle = restart_surface.get_rect(center=((self.width // 4) * 2, 9 * self.height // 10))
+        quit_rectangle = quit_surface.get_rect(center=((self._width // 4) * 3, 9 * self.height // 10))
+
+        self.screen.blit(reset_surface, reset_rectangle)
+        self.screen.blit(restart_surface, restart_rectangle)
+        self.screen.blit(quit_surface, quit_rectangle)
         pass
 
     def select(self, row, col):
@@ -53,6 +72,7 @@ class Board:
         j == col
         # declare cell as selected
         # check each column per row (so check each cell)
+
         for row in range(self.board_rows) # calls instance variable for length of rows
             for col in range(self.board_cols) # calls instance variable for length of cols
                 self.cells[row][col].selected = True # marked as selected
@@ -74,8 +94,8 @@ class Board:
         #     return selected_cell
 
     def click(self,x,y):
-        if x <= self.width and y <= self.height: #FIXME it should be in the tuple is in the screen
-            return self.cells[x][y]
+        if x in Board.width and y in Board.height: #FIXME it should be in the tuple is in the screen
+            return cell.(x,y)
         '''if a tuple of (x,y) coord is within the displayed board,
         this function returns a tuple of the (row,col) of the cell which was clicked'''
         return None
@@ -83,33 +103,22 @@ class Board:
     def clear(self):
         # Clears the value cell. Note that the user can only remove the cell values and sketched value that are
         # filled by themselves
-        for row in range(self.board_rows): # for every row check eveery column (check each cell)
-            for col in range(self.board_cols):
-                if self.cells[row][col].selected: # if selected
-                    # check OG board, if there is nothin in OG board, remove the sketched value and actual value
-                    if self.board[row][col] == 0:
-                        self.cells[row][col].sketched_value == 0
-                        self.cells[row][col].value == 0
+        pass
 
-
-    def sketch(self, value):
+    def sketch(selfself, value):
         # Sets the sketched value of the current selected cell equal to user entered value.
         # It will be displayed at the top left corner of the cell using the draw() function.
-
-        for row in range(self.board_rows):  # start how you do almost all the other functions
-            for col in range(self.board_cols): 
-                if self.cells[row][col].selected:
-                    self.cells[row][col].set_sketched_value(value)   # uses cell class setter function for setting sketched
+        pass
 
     def place_number(self,value):
         # sets the value of the current selected cell equal to user entered value
-        #called when the user presses the Enter key IN MAINNNNNN
-        for row in range(self.board_rows):    # start how you do almost all other functions (looking through each)
+        #called when the user presses the Enter key
+        for row in range(self.board_rows)
             for col in range(self.board_cols):
-                if self.cells[col][row].selected(): # if cell is selected and has nothing there but CAN have something there, sketch and set
-                    if self.board[col][row] == 0:
-                        self.cells[col][row].sketched_value[0]
-                        self.cells[col][row].set_cell_value()
+                if self.cells[col][row].selected()
+                    if self.board[col][row] = 0:
+                        self.cells[col][row].sketched_value 0:
+                        self.cells[col][row]set_cell_value():
 
     def reset_to_original(self):
         # Reset all cells in the board to their original values (0 if cleared, otherwise the corresponding digit)
